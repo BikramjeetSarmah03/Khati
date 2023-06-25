c21c27faeb98d0aa4b3d33cbc075d181a043d54a
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-06-25 13:01:21 +0530

    Responsive Hero Carousel

--- client/src/components/home/hero/HeroCarouselProduct.tsx
+++ client/src/components/home/hero/HeroCarouselProduct.tsx
@@ -14,12 +14,14 @@ export default function HeroCarouselProduct({}: CarouselProductProps) {
         />
       </div>
 
-      <div className="absolute right-0 z-50 w-1/2 h-full top-[25%] xl:top-1/2 space-y-4">
-        <h2 className="text-3xl uppercase text-black-light">Welcome to our</h2>
-        <h1 className="text-4xl font-bold uppercase text-black-dark">
+      <div className="absolute right-0 z-50 w-1/2 h-full top-[25%] xl:top-1/2 space-y-2 md:space-y-4">
+        <h2 className="text-lg uppercase md:text-3xl text-black-light">
+          Welcome to our
+        </h2>
+        <h1 className="text-xl font-bold uppercase md:text-4xl text-black-dark">
           Furniture
         </h1>
-        <p className="pr-8 break-words text-black-light">
+        <p className="pr-8 text-xs break-words line-clamp-4 md:line-clamp-[0] text-black-light md:text-base">
           Description Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Aliquid, nulla? Lorem ipsum dolor sit, amet consectetur adipisicing
           elit. Hic, optio.
