98b6c015edd2c93025cddb8f36fc950444cd3854
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-02 20:10:34 +0530

    Admin Initials

---
+++ admin/src/app/(root)/layout.tsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Khati: Admin",
+  description: "Admin Dashboard for Khati.vercel.app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ admin/src/app/(root)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="font-mono text-5xl text-center">
+      Khati: <span className="font-serif font-bold uppercase">Admin</span>
+    </main>
+  );
+}
