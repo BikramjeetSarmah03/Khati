d6b87c2f166c977a21d66ccc57e781c9f55f5ab8
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-03 23:17:19 +0530

    Clerk Auth

--- admin/src/app/(root)/page.tsx
+++ admin/src/app/(root)/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="font-mono text-5xl text-center">
       Khati: <span className="font-serif font-bold uppercase">Admin</span>
