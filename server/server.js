236d6e4d243c34a8245b19d648ce1518f6027915
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-04 10:26:15 +0530

    Server Initials

---
+++ server/server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const app = require("./app");
+const connectDatabase = require("./config/db");
+
+const PORT = process.env.PORT;
+app.listen(PORT, async () => {
+  await connectDatabase();
+  console.log(`Server started at PORT: ${PORT}`);
+});
