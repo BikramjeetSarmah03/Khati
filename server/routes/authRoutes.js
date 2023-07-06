2bf859a4bfc5e62e0a942a75b7a44f628a16800a
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-06 12:41:14 +0530

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { registerUser } = require("../controllers/userControllers");
-const router = express.Router();
-
-router.post("/register", registerUser);
-
-module.exports = router;
--- server/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-const connectDatabase = require("./config/db");
-
-const PORT = process.env.PORT;
-app.listen(PORT, async () => {
-  await connectDatabase();
-  console.log(`Server started at http://localhost:${PORT}`);
-});
