6f3cdb0260bed9d132a033b86a05b0d49bcbf076
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-06 12:40:50 +0530

    Delete admin directory

--- admin/src/app/(root)/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main className="font-mono text-5xl text-center">
-      Khati: <span className="font-serif font-bold uppercase">Admin</span>
-    </main>
-  );
-}
