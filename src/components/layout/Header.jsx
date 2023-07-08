994eaff6f52cd223eb75d10a97dd9d2ef1904849
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-08 23:45:57 +0530

    Removed session log

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,8 +9,6 @@ import { FiChevronDown, FiShoppingCart } from "react-icons/fi";
 export default function Header() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return (
     <header>
       <nav className="flex items-center justify-between p-4 space-x-4 shadow-xl lg:px-32">
