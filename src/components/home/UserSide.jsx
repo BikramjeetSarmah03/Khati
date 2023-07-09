d8768b4e03b8a1dadc7c543dc3610637c43ab55f
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-09 05:45:54 +0530

    Added User Session

--- src/components/home/UserSide.jsx
+++ src/components/home/UserSide.jsx
@@ -23,23 +23,38 @@ export default function UserSide() {
   return (
     <div className="relative flex flex-col h-full overflow-hidden bg-[url('/assets/userheader.jpg')] bg-no-repeat">
       <div className="flex flex-col items-center flex-grow mt-16 space-y-4">
-        <Image
-          src={
-            "https://res.cloudinary.com/bikramjeet/image/upload/v1688795542/Khati/avatars/guest-user_p5nqz0.png"
-          }
-          alt="userImg"
-          height={100}
-          width={100}
-        />
-
-        <div className="flex items-center">
-          <button className="px-8 py-2 mx-2 text-white rounded-xl hover:bg-gray-100 bg-gradient-to-br from-pink-500 to-orange-400 ">
-            Register
-          </button>
-          <button className="px-8 py-2 mx-2 rounded-xl hover:bg-gray-100 bg-gray-50">
-            Login
-          </button>
-        </div>
+        {session ? (
+          <div className="flex flex-col items-center justify-center w-full">
+            <Image
+              src={session.user.image}
+              alt="userImg"
+              height={70}
+              width={70}
+            />
+            <span className="mt-4 text-2xl font-bold text-center">
+              {session.user.name}
+            </span>
+          </div>
+        ) : (
+          <>
+            <Image
+              src={
+                "https://res.cloudinary.com/bikramjeet/image/upload/v1688795542/Khati/avatars/guest-user_p5nqz0.png"
+              }
+              alt="userImg"
+              height={70}
+              width={70}
+            />
+            <div className="flex items-center">
+              <button className="px-4 py-2 mx-2 text-white transition-all duration-300 lg:px-8 rounded-xl hover:bg-gray-100 bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-br hover:from-pink-600 hover:to-orange-500">
+                Register
+              </button>
+              <button className="px-4 py-2 mx-2 transition-all duration-300 bg-gray-200 lg:px-8 rounded-xl hover:bg-gray-300">
+                Login
+              </button>
+            </div>
+          </>
+        )}
 
         <ul className="flex items-center py-4 space-x-4 text-2xl lg:text-3xl">
           <li className="p-2 transition-all duration-300 rounded-full cursor-pointer hover:bg-gray-200">
