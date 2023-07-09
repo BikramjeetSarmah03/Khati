e98d54e650c8efe2cccbb4dd644f4e9fd8f08093
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-10 00:02:14 +0530

    Added Tailwind Debug Screens Plugin

--- src/app/(root)/layout.jsx
+++ src/app/(root)/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} debug-screens`}>
         <NextAuthProvider>
           <ReduxProvider>
             <Header />
