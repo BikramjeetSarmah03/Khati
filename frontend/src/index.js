7d180c958f59199e098e06209308150779246735
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-15 04:42:14 +0530

    Backend Cors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,15 +13,14 @@ ReactDOM.render(
       <SnackbarProvider
         maxSnack={2}
         anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-      >
+          vertical: "bottom",
+          horizontal: "center",
+        }}>
         <Router>
           <App />
         </Router>
       </SnackbarProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
