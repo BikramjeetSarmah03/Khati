e588807363d6da3de1c7ca376e27c3ee968d6875
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2023-07-16 23:12:20 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
   })
 );
