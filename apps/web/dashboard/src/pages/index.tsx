e4791477e21c9b7a6758f374c647ae7c1a4a3efe
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2024-10-21 00:06:28 +0530

    Featured Section setup

--- apps/web/dashboard/src/pages/index.tsx
+++ apps/web/dashboard/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { HeroSection } from "@/components/sections/home/hero-section";
+import { HeroSection } from "@/components/sections/hero/hero-section";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -8,8 +8,17 @@ export const Route = createFileRoute("/")({
 
 function Index() {
   return (
-    <div className="h-full p-2">
+    <div className="h-full p-2 space-y-20">
       <HeroSection />
+
+      <section className="mx-auto max-w-7xl">
+        <div>
+          <h1 className="flex flex-col items-center gap-4 text-4xl font-bold">
+            <span>Featured Products</span>
+            <hr className="w-20 h-0.5 bg-primary" />
+          </h1>
+        </div>
+      </section>
     </div>
   );
 }
